08357604e1a72a92ac2196cf62f78ef30c68e2ad
Ryan Chung <ryanchuung@gmail.com>
2020-08-26 16:50:04 -0700

    added Appointment model with custom manager for querying

--- scheduling/models.py
+++ scheduling/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+from applicants.models import Applicant
+from doctors.models import Doctor
+
+
+class AppointmentManager(models.Manager):
+
+    def upcoming_appointments(self, **kwargs):
+        return self.filter(appointment_date__gt=timezone.now())
+
+    def todays_appointments(self, **kwargs):
+        return self.filter(appointment_date=timezone.now().date())
+
+    def past_appointments(self, **kwargs):
+        return self.filter(appointment_date__lt=timezone.now())
+
+
+class Appointment(models.Model):
+    appointment_date = models.DateTimeField()
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
+    doctors = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.appointment_date}'
+
+    objects = models.Manager()
+    objects = AppointmentManager()
+
