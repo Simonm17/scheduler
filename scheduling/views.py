98cc0b083f668ba316f9cdabe32e961234c73aa0
Ryan Chung <ryanchuung@gmail.com>
2020-08-27 13:49:34 -0700

    modified listview queryset to show in template

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Appointment
@@ -8,3 +8,14 @@ from .models import Appointment
 class AppointmentListView(LoginRequiredMixin, ListView):
     model = Appointment
     template_name = 'scheduling/home.html'
+
+    def get_queryset(self):
+        """ 
+        Return list of appointments scheduled by currently logged in user. 
+        """
+        user = self.request.user
+        queryset = Appointment.objects.filter(created_by=user)
+        queryset = queryset.order_by('appointment_date')
+        return queryset
+
+
