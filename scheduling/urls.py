30dad12ac674419f3a1703588287f9c9191467ad
Ryan Chung <ryanchuung@gmail.com>
2020-08-28 04:55:28 -0700

    created appointment create function & routed, added todo in appointmentlistview

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     AppointmentListView,
     AppointmentDetailView,
+    schedule,
 )
 
 
@@ -9,5 +10,6 @@ app_name = 'scheduling'
 
 urlpatterns = [
     path('', AppointmentListView.as_view(), name='home'),
+    path('appointment/new/', schedule, name='new_appointment'),
     path('appointment/<int:pk>/', AppointmentDetailView.as_view(), name='appointment'),
 ]
--- scheduling/views.py
+++ scheduling/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import (
     ListView, 
     DetailView,
@@ -6,11 +6,24 @@ from django.views.generic import (
     UpdateView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Appointment
+from .forms import NewAppointmentForm
+from applicants.models import Applicant
+from applicants.forms import NewApplicantForm
+from doctors.models import Doctor
+from doctors.forms import NewDoctorForm
 
 
 class AppointmentListView(LoginRequiredMixin, ListView):
+    """
+    TODO:   Current queryset displays all appts created by user.
+            We need to have some more filters 
+            to separate past and upcoming appts.
+            Maybe within template conditions?
+    """
     model = Appointment
     template_name = 'scheduling/home.html'
 
@@ -35,3 +48,38 @@ class AppointmentDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView)
         if user.is_staff or user == appointment.created_by:
             return True
         return False
+
+@login_required
+def schedule(request):
+    if request.method == 'POST':
+        applicant_form = NewApplicantForm(request.POST, prefix='a_form')
+        doctor_form = NewDoctorForm(request.POST, prefix='d_form')
+        scheduling_form = NewAppointmentForm(request.POST, prefix='s_form')
+        if applicant_form.is_valid() and doctor_form.is_valid() and scheduling_form.is_valid():
+            # save applicant object
+            applicant = applicant_form.save(commit=False)
+            applicant.created_by = request.user
+            applicant.save()
+            # save doctor object
+            doctor = doctor_form.save(commit=False)
+            doctor.created_by = request.user
+            doctor.save()
+            # save appointment object with app/doc instance
+            appointment = scheduling_form.save(commit=False)
+            appointment.applicant = applicant
+            appointment.doctor = doctor
+            appointment.created_by = request.user
+            appointment.save()
+            messages.success(request, f'Your appointment is scheduled.')
+            # redirect user to appointment object's detailview
+            return redirect('scheduling:appointment', appointment.pk)
+    else:
+        applicant_form = NewApplicantForm(prefix='a_form')
+        doctor_form = NewDoctorForm(prefix='d_form')
+        scheduling_form = NewAppointmentForm(prefix='s_form')
+    return render(request, 'scheduling/new_appointment.html', context={
+        'a_form': applicant_form,
+        'd_form': doctor_form,
+        's_form': scheduling_form
+        }
+    )
