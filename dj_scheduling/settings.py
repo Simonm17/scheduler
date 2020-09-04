1d489a028addb032ff4e204cd3980bc971f4edd4
Ryan Chung <ryanchuung@gmail.com>
2020-09-03 20:04:58 -0700

    modified static_root again

--- dj_scheduling/settings.py
+++ dj_scheduling/settings.py
@@ -146,10 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # static root for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
+  os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
