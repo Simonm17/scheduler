82d38a0c4aef85d354a9f1ab6d77968d86a984d2
Ryan Chung <ryanchuung@gmail.com>
2020-09-03 17:55:03 -0700

    modified static_root settings and url

--- dj_scheduling/settings.py
+++ dj_scheduling/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 
 # static root for production
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
