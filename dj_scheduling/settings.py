705d81be92079137aa00e36274e0032623236000
Ryan Chung <ryanchuung@gmail.com>
2020-09-03 20:36:54 -0700

    modified static root settings

--- dj_scheduling/settings.py
+++ dj_scheduling/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # static root for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
