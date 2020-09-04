d1db1458353d7d04205b3a2ad04fe0b53eaffb8f
Ryan Chung <ryanchuung@gmail.com>
2020-09-03 20:07:41 -0700

    modified config/urls.py

--- dj_scheduling/urls.py
+++ dj_scheduling/urls.py
@@ -12,6 +12,5 @@ urlpatterns = [
     path('doctors/', include('doctors.urls', namespace='doctors')),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
